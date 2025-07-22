7122d2ef13edc0f82696f186840f7e6f5dbff44d
Anusha Reddy <123926678+AnushaReddy25@users.noreply.github.com>
2025-07-22 16:30:58 +0530

    Update key-pair-main.tf

--- key-pair-main.tf
+++ key-pair-main.tf
@@ -5,7 +5,7 @@ resource "tls_private_key" "key_pair" {
 }
 # Create the Key Pair
 resource "aws_key_pair" "key_pair" {
-  key_name   = "anukeypairec27"  
+  key_name   = "anukeypairec275"  
   public_key = tls_private_key.key_pair.public_key_openssh
 }
 # Save file
